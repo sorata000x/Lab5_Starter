2d1e312183a983d613fa849bcc127a9c99ebf8a1
Sora <70079892+sorata000x@users.noreply.github.com>
2023-05-08 21:48:45 -0700

    Completed explore

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Lab 5 - Starter
 
 Name: Aiden Chen
+
+[introduction-to-github](https://github.com/sorata000x/introduction-to-github/tree/main)
--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -2,6 +2,62 @@
 
 window.addEventListener('DOMContentLoaded', init);
 
+const synth = window.speechSynthesis;
+const voiceSelect = document.getElementById("voice-select");
+const inputTxt = document.getElementById("text-to-speak");
+
 function init() {
-  // TODO
+  setTimeout(() => { populateVoiceList();}, 10);
+  pressToTalk();
+}
+
+const inputForm = document.querySelector("form");
+
+let voices = [];
+
+function populateVoiceList() {
+    voices = synth.getVoices();
+    for (let i = 0; i < voices.length; i++) {
+      const option = document.createElement("option");
+      option.textContent = `${voices[i].name} (${voices[i].lang})`;
+      if (voices[i].default) {
+        option.textContent += " — DEFAULT";
+      }
+      option.setAttribute("data-lang", voices[i].lang);
+      option.setAttribute("data-name", voices[i].name);
+      voiceSelect.appendChild(option);
+    }  
+}
+
+function pressToTalk() {
+  let button = document.querySelector('button');
+
+  button.addEventListener('click', function(event) {
+    event.preventDefault();
+    speakInput();
+  })
+}
+
+function speakInput() {
+  const utterThis = new SpeechSynthesisUtterance(inputTxt.value);
+  utterThis.addEventListener('start', function() {
+    setFace("assets/images/smiling-open.png");
+  })
+  utterThis.addEventListener('end', function() {
+    setFace("assets/images/smiling.png");
+  })
+  const selectedOption =
+    voiceSelect.selectedOptions[0].getAttribute("data-name");
+  for (let i = 0; i < voices.length; i++) {
+    if (voices[i].name === selectedOption) {
+      utterThis.voice = voices[i];
+    }
+  }
+  synth.speak(utterThis);
+  inputTxt.blur();
+}
+
+function setFace(src) {
+  let face = document.querySelector('img[src^="assets/images/"]');
+  face.src = src;
 }
